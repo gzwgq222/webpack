ad06d9b5197a3dbc827cb85848d8a7e22a6a06c5
gq <gq@allpyra.com>
2018-11-09 15:51:04 +0800

    book

--- src/two.js
+++ src/two.js
@@ -3,5 +3,14 @@ function two() {
   element.innerHTML = '我是第二个入口文件';
   return element;
 }
+// 求数组中的最大 最小值
+const max = Math.max.apply(null, [1,2,3,4,5,99])
+const min = Math.min.apply(null, [1,2,3,4,5,99])
+console.log('max：' + max)
+console.log('min：' + min)
 
+// 合并两个数组
+const arr1 = [1,2,3]
+const arr2 = [4,5,6]
+const cont = Array.prototype.push.apply(arr1, arr2)
 document.getElementById('root').appendChild(two());
