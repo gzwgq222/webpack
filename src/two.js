ac1f1fe07207f7078462b1c5c48827c58bb94896
gq <gq@allpyra.com>
2019-01-05 23:56:39 +0800

    async

--- src/two.js
+++ src/two.js
@@ -3,24 +3,7 @@ function two() {
   element.innerHTML = '第二个入口文件';
   return element;
 }
-function throttle(fn, gapTime) {
-  let _lastTime = null;
-  console.log(888)
-  return function () {
-    let _nowTime = + new Date()
-    console.log(_nowTime, _lastTime)
-    if (_nowTime - _lastTime > gapTime || !_lastTime) {
-      fn();
-      _lastTime = _nowTime
-    }
-  }
-}
 
-let fn = ()=>{
-  console.log('boom')
-}
-// throttle(fn, 1000)
-// setInterval(throttle(fn,5000), 3000)
 
 // 求数组中的最大 最小值
 const max = Math.max.apply(null, [1,2,3,4,5,99])
@@ -28,8 +11,16 @@ const min = Math.min.apply(null, [1,2,3,4,5,99])
 console.log('max：' + max)
 console.log('min：' + min)
 
-// 合并两个数组
-const arr1 = [1,2,3]
-const arr2 = [4,5,6]
-const cont = Array.prototype.push.apply(arr1, arr2)
+// async await
+
+async function f() {
+  let promise = new Promise((resolve, reject) => {
+      setTimeout(() => resolve('done!'), 2000)
+  })
+  console.log(1)
+  let result = await promise // 直到promise返回一个resolve值（*）
+  console.log(result) // 'done!' 
+}
+f()
+
 document.getElementById('root').appendChild(two());
